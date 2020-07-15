df0a015901b02d08318deb6d5c1d47886a508320
Crowdbotics <team@crowdbotics.com>
2020-07-15 11:32:28 +0000

    Update components

--- src/features/BlankScreen38945/screens/blank.js
+++ src/features/BlankScreen38945/screens/blank.js
@@ -167,7 +167,7 @@ export class _Blank extends React.Component {
       >
         <View
           style={{
-            width: "50%",
+            width: "33.333333333333336%",
             height: "100%",
             marginLeft: 0,
             marginRight: 0,
@@ -201,37 +201,6 @@ export class _Blank extends React.Component {
             backgroundSize: "auto"
           }}
         >
-          <Datepicker
-            style={{
-              width: "100%",
-              marginLeft: 0,
-              marginRight: 0,
-              marginTop: 5,
-              marginBottom: 5,
-              paddingLeft: 5,
-              paddingRight: 5,
-              paddingTop: 5,
-              paddingBottom: 5,
-              overflow: "visible",
-              textAlign: "left",
-              verticalAlign: "baseline",
-              fontSize: 20,
-              color: "#000000",
-              backgroundColor: "#ffffff",
-              fontStyle: "normal",
-              fontWeight: "normal",
-              borderColor: "#000000",
-              borderStyle: "solid",
-              borderWidth: 0,
-              borderLeftWidth: 0,
-              borderRightWidth: 0,
-              borderTopWidth: 0,
-              borderBottomWidth: 0,
-              borderRadius: 0
-            }}
-            date={this.state.Datepicker_16}
-            onSelect={nextValue => this.setState({ Datepicker_16: nextValue })}
-          />
           <Button
             style={{
               width: "100%",
@@ -264,10 +233,41 @@ export class _Blank extends React.Component {
           >
             Press me!
           </Button>
+          <Datepicker
+            style={{
+              width: "100%",
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 5,
+              marginBottom: 5,
+              paddingLeft: 5,
+              paddingRight: 5,
+              paddingTop: 5,
+              paddingBottom: 5,
+              overflow: "visible",
+              textAlign: "left",
+              verticalAlign: "baseline",
+              fontSize: 20,
+              color: "#000000",
+              backgroundColor: "#ffffff",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0
+            }}
+            date={this.state.Datepicker_16}
+            onSelect={nextValue => this.setState({ Datepicker_16: nextValue })}
+          />
         </View>
         <View
           style={{
-            width: "50%",
+            width: "33.333333333333336%",
             height: "100%",
             marginLeft: 0,
             marginRight: 0,
@@ -335,6 +335,43 @@ export class _Blank extends React.Component {
             value={this.state.Input_19}
             onChangeText={nextValue => this.setState({ Input_19: nextValue })}
           />
+        </View>
+        <View
+          style={{
+            width: "33.333333333333336%",
+            height: "100%",
+            marginLeft: 0,
+            marginRight: 0,
+            marginTop: 0,
+            marginBottom: 0,
+            paddingLeft: 0,
+            paddingRight: 0,
+            paddingTop: 0,
+            paddingBottom: 0,
+            overflow: "visible",
+            textAlign: "left",
+            verticalAlign: "baseline",
+            fontSize: 20,
+            color: "#000000",
+            backgroundColor: "#ffffff",
+            fontStyle: "normal",
+            fontWeight: "normal",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+            alignItems: "stretch",
+            alignContent: "stretch",
+            borderColor: "#000000",
+            borderStyle: "solid",
+            borderWidth: 0,
+            borderLeftWidth: 0,
+            borderRightWidth: 0,
+            borderTopWidth: 0,
+            borderBottomWidth: 0,
+            borderRadius: 0,
+            backgroundSize: "auto"
+          }}
+        >
           <Datepicker
             style={{
               width: "100%",
